e4c94af9580e51558295002a9e2796a75024df83
Tarik <tarik.jaidani@gmail.com>
2026-03-18 22:43:14 +0100

    push

--- Cpp_Level6.cpp
+++ Cpp_Level6.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
 
+ 
 
-void check( )
+void check()
 {
-	 
+	string result;
+	int  Number = 0;
+
+	  
+	result = (Number > 0) ? "Postive" : ((Number == 0) ? "Zero" : "Negative");
+
+	cout << "\nResult is: " << result << "\n";
+
+
 }
 int main()
-{
- 
+{ 
+	check();
 }
