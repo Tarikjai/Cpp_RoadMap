2b624e1fb51cacf03ca4bc5ea084d2df66509aaf
Tarik <tarik.jaidani@gmail.com>
2026-03-19 22:33:33 +0100

    push

--- 09.cpp
+++ 09.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 using namespace std;
 
-
-
 void check()
 {
 	string result;
 	int  Number = 0;
-
-
 	result = (Number > 0) ? "Postive" : ((Number == 0) ? "Zero" : "Negative");
 
 	cout << "\nResult is: " << result << "\n";
-
 
 }
 int main()
