acb2514741006906ecf5d5ad9d7b75ed562bc09c
Tarik <tarik.jaidani@gmail.com>
2026-03-22 14:33:03 +0100

    push

---
+++ 25.cpp
@@ -0,0 +1,22 @@
+#include <iostream>;
+
+using namespace std;
+
+
+int main() {
+
+	char Name[] = "Tarik Jaidani";
+	char SchoolName[] = "Programming Advices";
+
+	printf("Hello %s How do you do ?\n", Name);
+	printf("Welcome do %s school \n", SchoolName);
+ 
+	char c = 'S';
+
+	printf("\nSetting the with of cat :%*C \n", 1 , c);
+	printf("\nSetting the with of cat :%*c \n", 2, c);
+	printf("\nSetting the with of cat :%*C \n", 3, c);
+	printf("\nSetting the with of cat :%*c \n", 4, c);
+	 
+
+}
