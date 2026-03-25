b6bff6233ef87c67dcaede561a3aca1898f93579
Tarik <tarik.jaidani@gmail.com>
2026-03-25 22:51:40 +0100

    push

--- 29.cpp
+++ 29.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main() {
 
 	vector <int> vNumber;
-
 	vNumber.push_back(10);
 	vNumber.push_back(20);
 	vNumber.push_back(30);
@@ -18,9 +17,8 @@ int main() {
 	for (int& Number : vNumber) {
 		cout << Number << endl;
 	}
+
 	cout << endl;
-
-
 
 }
 //2 functioon ,  readNumbers(int vector) => ask to enter number
