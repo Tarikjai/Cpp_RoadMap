048be316e54acd14d73948ce436f4182cb62954f
Tarik <tarik.jaidani@gmail.com>
2026-04-06 19:32:14 +0200

    push

---
+++ 54.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+void printFileContent(string fileName) {
+
+	fstream Myfile;
+
+	Myfile.open(fileName, ios::in);
+
+	if (Myfile.is_open()) {
+
+		string Line;
+
+		while (getline(Myfile, Line)) {
+
+			cout << Line << endl;
+		}
+		Myfile.close();
+	}
+
+
+}
+
+int main() {
+
+	printFileContent("text.txt");
+
+
+}
