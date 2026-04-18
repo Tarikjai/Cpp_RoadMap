21b779f6b63857505c48338402fd2bd6048a6896
Tarik <tarik.jaidani@gmail.com>
2026-04-18 14:08:10 +0200

    push

--- 01.cpp
+++ 01.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
 
-#include <iomanip>
-
 using namespace std;
 
 
-int  RandomNumber(int From, int To)
-{
-	int RandomNumber = rand() % (To - From + 1) + From;
-	return RandomNumber;
-}
-
-
-void GenerateMatrix() {
-
-	for (int i = 0; i < 3; i++) {
-		for (int i = 0; i < 3; i++) {
-			cout << RandomNumber(1, 1000) << setw(5);
-		} 
-		cout << setw(-1)<< endl;
-	}
-	
-}
-
 int main() {
 
-
-	cout << "The following is a 3x3  random matrix: " << endl;
-
-	GenerateMatrix();
-
-
+	cout << "Hello";
 }
