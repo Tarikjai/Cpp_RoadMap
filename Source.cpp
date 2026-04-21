3690877678a99f415bc426244855719ee2cd9856
Tarik <tarik.jaidani@gmail.com>
2026-04-21 22:03:24 +0200

    push

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+#include "MyLib.h"
+
+using namespace std;
+
+
+void GenerateRandomNumber(int arr[3][3], int Rows, int Column) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Column; j++) {
+			arr[i][j] = MyLib::RandomNumber(1, 100);
+		}
+
+	}
+
+}
+
+void PrintRandomNumber(int arr[3][3], int Rows, int Column) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Column; j++) {
+			cout << setw(3) << arr[i][j] << "   ";
+		}
+		cout << endl;
+
+	}
+
+}
+
+int RowSum(int arr[3][3], int i, int Column) {
+
+	int sum = 0;
+	for (int j = 0; j <= Column - 1; j++) {
+		sum += arr[i][j];
+	}
+
+	return sum;
+}
+
+
+void PrintSumRows(int arr[3][3], int Rows, int Column) {
+	cout << "\nThe following are the sum  for each row in the matrix :\n";
+	for (int i = 0; i < Rows; i++) {
+		cout << " Row " << i + 1 << " Sum = " << RowSum(arr, i, Column);
+		cout << endl;
+	}
+}
+
+
+int main() {
+
+	int arr[3][3];
+
+
+	GenerateRandomNumber(arr, 3, 3);
+	cout << "\n The following is a random matrix:\n";
+	PrintRandomNumber(arr, 3, 3);
+
+
+	PrintSumRows(arr, 3, 3);
+
+}
