8d83ba583a4f563e67dd842f3715721f2675627c
Tarik <tarik.jaidani@gmail.com>
2026-04-22 20:38:46 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -6,17 +6,33 @@ using namespace std;
 
 void FillArrayWithRadom3X3(int arr[3][3], int Rows, int Cols) {
 
-
-
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+			arr[i][j] = MyLib::RandomNumber(1, 100);
+		}
+	}
 }
+ 
+void printRadom3X3(int arr[3][3], int Rows, int Cols) {
+
+	cout << "\nThe following is a 3x3 random matrix:\n";
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+			cout << setw(3) << arr[i][j] << "   ";
+		}
+		cout << endl;
+	}
+}
+
+
 
 
 int main() {
 
 	int arr[3][3];
 
-	FillArrayWithRadom3X3(arr,3,3) {
+	FillArrayWithRadom3X3(arr, 3, 3);
 
-	}
+	printRadom3X3(arr, 3, 3);
 
 }
