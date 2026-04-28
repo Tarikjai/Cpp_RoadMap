80947d10f23467f5cda047e28f5e4297eaa5303c
Tarik <tarik.jaidani@gmail.com>
2026-04-28 22:27:23 +0200

    push

--- 11.cpp
+++ 11.cpp
@@ -4,21 +4,21 @@
 
 using namespace std;
 
-void FillMatrixWithRandomNumbers(int arr1[3][3], int Rows, int Cols) {
+void FillMatrixWithRandomNumbers(int Arr[3][3], int Rows, int Cols) {
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Cols; j++) {
-			arr1[i][j] = MyLib::RandomNumber(1, 11);
+			Arr[i][j] = MyLib::RandomNumber(1, 11);
 		}
 	}
 }
 
 
-void Print3x3Matrix(int arr[3][3], int Rows, int Cols) {
+void Print3x3Matrix(int Arr[3][3], int Rows, int Cols) {
 
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Cols; j++) {
 			//cout << setw(3) << arr[i][j] << "  ";
-			printf(" %0*d   ", 2, arr[i][j]);
+			printf(" %0*d   ", 2, Arr[i][j]);
 		}
 		cout << endl;
 	}
@@ -26,52 +26,50 @@ void Print3x3Matrix(int arr[3][3], int Rows, int Cols) {
 }
 
 
-int SumOfMatrix(int Matrix[3][3], short Rows, short Cols)
+int SumOfMatrix(int Arr[3][3], short Rows, short Cols)
 {
 	int Sum = 0;
 	for (short i = 0; i < Rows; i++)
 	{
 		for (short j = 0; j < Cols; j++)
 		{
-			Sum += Matrix[i][j];
+			Sum += Arr[i][j];
 		}
 	}
 	return Sum;
 }
 
 
-bool checkEqual(int SumArr1, int SumArr2) {
+bool checkEqual(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Cols) {
 
-	return(SumArr1 == SumArr2);
+	return(SumOfMatrix(Matrix1,  Rows,  Cols) == SumOfMatrix(Matrix2,  Rows,  Cols));
 }
 
 
-string Answer(int Result) {
-	if (Result) {
-		return "Yes, equal";
-	}else{
-		return "No: matrices are not equal";
-	}
 
-}
 
  
 int main() {
 
-	int arr1[3][3];
-	int arr2[3][3];
+	int Matrix1[3][3];
+	int Matrix2[3][3];
 
-	FillMatrixWithRandomNumbers(arr1, 3, 3);
-	FillMatrixWithRandomNumbers(arr2, 3, 3);
+	FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+	FillMatrixWithRandomNumbers(Matrix2, 3, 3);
 
 	cout << "Matrix1: " << endl;
-	Print3x3Matrix(arr1, 3, 3);
+	Print3x3Matrix(Matrix1, 3, 3);
 	cout << "Matrix2: " << endl;
-	Print3x3Matrix(arr2, 3, 3);
+	Print3x3Matrix(Matrix2, 3, 3);
 
-	int result = checkEqual(SumOfMatrix(arr1, 3, 3), SumOfMatrix(arr2, 3, 3));
+	if (checkEqual(Matrix1, Matrix2, 3, 3)) {
+		cout << "Yes, equal";
+	}
+	else {
+		cout << "No: matrices are not equal";
+	}
 
-	cout << Answer(result);
+	
 
 	
 
