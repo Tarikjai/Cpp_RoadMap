7e528b1ba6260fa8e7ea84a209cfb0594363de05
Tarik <tarik.jaidani@gmail.com>
2026-05-02 02:16:06 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -4,13 +4,7 @@
 
 using namespace std;
 
-void FillMatrixWithRandomNumbers(int Arr[3][3], int Rows, int Cols) {
-	for (int i = 0; i < Rows; i++) {
-		for (int j = 0; j < Cols; j++) {
-			Arr[i][j] = MyLib::RandomNumber(1, 11);
-		}
-	}
-}
+ 
 void Print3x3Matrix(int Arr[3][3], int Rows, int Cols) {
 
 	for (int i = 0; i < Rows; i++) {
@@ -24,22 +18,31 @@ void Print3x3Matrix(int Arr[3][3], int Rows, int Cols) {
 }
 
 
+bool CheckBigger(int Zero, int Positife) {
 
-int CountNumberInMatrix(int Matrix1[3][3], int Rows, int Cols, int CountNumber) {
+	return (Zero > Positife);
 
-	int counter=0;
+}
+
+
+int IsSparseMatrix(int Matrix1[3][3], int Rows, int Cols, int CountNumber=0) {
+
+	int countZero=0, countPositive=0;
 
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Rows; j++) {
 
-			if (Matrix1[i][j] == CountNumber) {
-				counter++;
+			if (Matrix1[i][j] == 0) {
+				countZero++;
+			}
+			else {
+				countPositive++;
 			}
 
 		}
 	}
 
-	return counter;
+	return CheckBigger(countZero, countPositive);
  }
 
 
@@ -47,16 +50,20 @@ int CountNumberInMatrix(int Matrix1[3][3], int Rows, int Cols, int CountNumber) 
 
 
 int main() {
-	int Matrix1[3][3];
 
-	FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+	int Matrix1[3][3] = { {1,0,0}, {0,1,0}, {1,1,1} };
+ 
 
 	cout << "Matrix1: " << endl;
 	Print3x3Matrix(Matrix1, 3, 3);
+	 
 
-	int CountNumber = MyLib::AskNumber("\nEnter the number to count in matrix? ");
-
-	cout << "Number "<< CountNumber << " count in matrix is: "<< CountNumberInMatrix(Matrix1, 3, 3, CountNumber);
+	if (IsSparseMatrix(Matrix1, 3, 3)) {
+		cout << "\nYes, It is sparce";   
+	}
+	else {
+		cout << "\nNo, It's NOT sparce";
+	}
 
 
 
