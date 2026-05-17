a98c609e253b47cb89bd470e61bba0253f8dfcf6
Tarik <tarik.jaidani@gmail.com>
2026-05-17 18:45:53 +0200

    push

--- 23_Print_First_String.cpp
+++ 23_Print_First_String.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
 #include "MyLib.h"
 
+using namespace std;
 
+void Generate3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+			Matrix[i][j] = MyLib::RandomNumber(1,100);
+		}
+	
+	}
+
+}
+
+void Print3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+			cout << setw(3) << Matrix[i][j] << "  ";
+		}
+		cout << endl;
+	}
+
+}
 int main()
 {
-	cout << "The fo";
+	int Matrix[3][3];
+	Generate3x3RandomMatrix(Matrix, 3, 3);
 
+	cout << "The following is a 3X3 random matrix: "  << endl;
+	Print3x3RandomMatrix(Matrix, 3, 3);
 }
