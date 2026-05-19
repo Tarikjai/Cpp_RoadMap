682591e8b754959c15764a3cd0a1b3f6975ec858
Tarik <tarik.jaidani@gmail.com>
2026-05-19 21:23:20 +0200

    push

--- 23_Print_First_String.cpp
+++ 23_Print_First_String.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+
 void Generate3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
 
 	for (int i = 0; i < Rows; i++) {
@@ -13,23 +15,49 @@ void Generate3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
 	
 	}
 
-}
 
-void Print3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
-
-	for (int i = 0; i < Rows; i++) {
-		for (int j = 0; j < Rows; j++) {
-			cout << setw(3) << Matrix[i][j] << "  ";
-		}
-		cout << endl;
-	}
-
-}
-int main()
-{
+int main() {
 	int Matrix[3][3];
 	Generate3x3RandomMatrix(Matrix, 3, 3);
 
 	cout << "The following is a 3X3 random matrix: "  << endl;
 	Print3x3RandomMatrix(Matrix, 3, 3);
 }
+
+
+
+
+
+
+
+
+//1
+//void Generate3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
+//
+//	for (int i = 0; i < Rows; i++) {
+//		for (int j = 0; j < Rows; j++) {
+//			Matrix[i][j] = MyLib::RandomNumber(1,100);
+//		}
+//	
+//	}
+//
+//}
+//
+//void Print3x3RandomMatrix(int Matrix[3][3], int Rows, int Cols) {
+//
+//	for (int i = 0; i < Rows; i++) {
+//		for (int j = 0; j < Rows; j++) {
+//			cout << setw(3) << Matrix[i][j] << "  ";
+//		}
+//		cout << endl;
+//	}
+//
+//}
+//int main()
+//{
+//	int Matrix[3][3];
+//	Generate3x3RandomMatrix(Matrix, 3, 3);
+//
+//	cout << "The following is a 3X3 random matrix: "  << endl;
+//	Print3x3RandomMatrix(Matrix, 3, 3);
+//}
