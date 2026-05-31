4eabbcd589fce87bddba1f6573694c3a7e30cad6
Tarik <tarik.jaidani@gmail.com>
2026-05-31 21:53:24 +0200

    push

--- 23_Print_First_String.cpp
+++ 23_Print_First_String.cpp
@@ -4,6 +4,62 @@
 
 using namespace std;
 
+
+void PrintMatrix(int arr[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			printf(" %0*d ", 2, arr[i][j]);
+		}
+		cout << endl;
+	}
+}
+
+int CountNumberInMatrix(int arr1[3][3], int Rows, int Cols, int Number) {
+	int count = 0;
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+			if (Number == arr1[i][j]) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+bool IsSparseMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+	short MatrixSize = Rows * Cols;
+	return (CountNumberInMatrix(Matrix1, 0, 3, 3) >= ceil((float)MatrixSize / 2));
+}
+
+
+
+int main() {
+
+	int arr1[3][3] = { {1,0,0},{0,1,0},{1,1,1} };
+
+	cout << "Matrix1:" << endl;
+	PrintMatrix(arr1, 3, 3);
+
+
+
+	 
+
+
+	if (IsSparseMatrix(arr1, 3, 3)) {
+		cout << "Yes, Matrix is sparce";
+	}
+	else {
+		cout << "No, Not sparce";
+	}
+
+	system("pause>0");
+}
+
+/* 15
+
 void PrintMatrix(int arr[3][3], int Rows, int Cols) {
 
 	for (int i = 0; i < Rows; i++) {
