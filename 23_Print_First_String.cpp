0f1a3384b210122f86c424f94ae576332c1fa2b5
Tarik <tarik.jaidani@gmail.com>
2026-06-11 23:29:34 +0200

    push

--- 23_Print_First_String.cpp
+++ 23_Print_First_String.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void PrintMatrix(int arr[3][3], int Rows, int Cols) {
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Cols; j++) {
-			printf(" %0*d  ", 2, arr[i][j]);
+			printf(" %0*d    ", 2, arr[i][j]);
 		}
 		cout << endl;
 	}
