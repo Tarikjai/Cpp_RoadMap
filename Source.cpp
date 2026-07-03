31a3758adf546268a97b48c28a8a84a475894693
Tarik <tarik.jaidani@gmail.com>
2026-07-03 19:13:12 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -5,28 +5,63 @@
 using namespace std;
 
 
+enum enWhatToCount { capitalLetter = 1, SmallLetters = 2, all = 3 };
 
- 
-
-string InvertAllLeters(string Texte) {
+int CountUpperLetters(string s1) {
+	int count = 0;
 	for (int i = 0; i < Texte.length(); i++) {
-		Texte[i] =  MyLib::invertChar(Texte[i]);
+		if (isupper(Texte[i])) {
+			count++;
+		}
 	}
-	return Texte;
+	return count;
+}
+
+int CountLowerLetters(string s1) {
+	int count = 0;
+	for (int i = 0; i < Texte.length(); i++) {
+		if (islower(Texte[i])) {
+			count++;
+		}
+	}
+	return count;
+}
+
+
+int countLetters(string s1, enWhatToCount WhattoCount) {
+
+
+	if (enWhatToCount::capitalLetter) {
+		return CountUpperLetters(s1);
+	}
+	if (enWhatToCount::SmallLetters) {
+		return CountLowerLetters(s1);
+	}
 }
 
 
 
 
+
+
 int main() {
-	string   Texte = MyLib::AskString("Please  Enter your string?\n");
+
+	string   s1 = MyLib::AskString("Please  Enter your string?\n");
+
 
 	cout << "\n";
 
-	cout << "\String after Inverting all Letters Case : \n"  ;
-	 
-	cout << InvertAllLeters(Texte);
-	
+
+	cout << "\nString length = " << s1.length();
+
+
+	cout << "\nCapital Letteres number = " << countLetters(s1, enWhatToCount::capitalLetter);
+
+
+	cout << "\nLower Letter number = " << countLetters(s1, enWhatToCount::SmallLetters);
+
+
 
 	system("pause>0");
+
 }
