35c7fe3c9c32934f6049fc0604b2c7c3cfe557d9
Tarik <tarik.jaidani@gmail.com>
2026-07-05 18:36:49 +0200

    push

--- 29_Count_Small_Capital_letters.cpp
+++ 29_Count_Small_Capital_letters.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include <iostream>
 using namespace std;
+
+
+
 string ReadString()
 {
 	string S1;
@@ -8,7 +11,11 @@ string ReadString()
 	getline(cin, S1);
 	return S1;
 }
+
+
 enum enWhatToCount { SmallLetters = 0, CapitalLetters = 1, All = 3 };
+
+
 short CountLetters(string S1, enWhatToCount
 	WhatToCount = enWhatToCount::All)
 {
@@ -19,18 +26,15 @@ short CountLetters(string S1, enWhatToCount
 	short Counter = 0;
 	for (short i = 0; i < S1.length(); i++)
 	{
-		if (WhatToCount == enWhatToCount::CapitalLetters &&
-			isupper(S1[i]))
+		if (WhatToCount == enWhatToCount::CapitalLetters &&	isupper(S1[i]))
 			Counter++;
-		if (WhatToCount == enWhatToCount::SmallLetters &&
-			islower(S1[i]))
+		if (WhatToCount == enWhatToCount::SmallLetters &&		islower(S1[i]))
 			Counter++;
 	}
 	return Counter;
 }
-ProgrammingAdvices.com
-© Copyright 2022
-Problem # 29 / 3 Solution Using C++
+
+
 short CountCapitalLetters(string S1)
 {
 	short Counter = 0;
@@ -51,20 +55,20 @@ short CountSmallLetters(string S1)
 	}
 	return Counter;
 }
-ProgrammingAdvices.com
-© Copyright 2022
-Problem # 29 / 3 Solution Using C++
+
+
 int main()
 {
 	string S1 = ReadString();
 	cout << "\nString Length = " << S1.length();
-	cout << "\nCapital Letters Count= " <<
-		CountCapitalLetters(S1);
+	cout << "\nCapital Letters Count= " <<		CountCapitalLetters(S1);
 	cout << "\nSmall Letters Count= " << CountSmallLetters(S1);
+
+
+
 	cout << "\n\nMethod 2\n";
+
 	cout << "\nString Length = " << CountLetters(S1);
-	cout << "\nCapital Letters Count= " << CountLetters(S1,
-		enWhatToCount::CapitalLetters);
-	cout << "\nSmall Letters Count= " <<
-		CountLetters(S1, enWhatToCount::SmallLetters);
+	cout << "\nCapital Letters Count= " << CountLetters(S1,		enWhatToCount::CapitalLetters);
+	cout << "\nSmall Letters Count= " <<CountLetters(S1, enWhatToCount::SmallLetters);
 	system("pause>0");
--- Source.cpp
+++ Source.cpp
@@ -5,43 +5,19 @@
 using namespace std;
 
 
-enum enWhatToCount { capitalLetter = 1, SmallLetters = 2, all = 3 };
 
-int CountUpperLetters(string s1) {
+
+int CountLetter(string S1, char C) {
 	int count = 0;
-	for (int i = 0; i < s1.length(); i++) {
-		if (isupper(s1[i])) {
+
+	for (int i = 0; i<S1.length(); i++) {
+		if (S1[i] == C) {
 			count++;
 		}
 	}
 	return count;
 }
 
-int CountLowerLetters(string s1) {
-	int count = 0;
-	for (int i = 0; i < s1.length(); i++) {
-		if (islower(s1[i])) {
-			count++;
-		}
-	}
-	return count;
-}
-
-
-int countLetters(string s1, enWhatToCount WhattoCount=enWhatToCount::all) {
-
-
-	if (WhattoCount == enWhatToCount::all) {
-		return s1.length();
-	}
-
-	if (WhattoCount== enWhatToCount::capitalLetter) {
-		return CountUpperLetters(s1);
-	}
-	if (WhattoCount == enWhatToCount::SmallLetters) {
-		return CountLowerLetters(s1);
-	}
-}
 
 
 
@@ -50,19 +26,15 @@ int countLetters(string s1, enWhatToCount WhattoCount=enWhatToCount::all) {
 
 int main() {
 
-	string   s1 = MyLib::AskString("Please  Enter your string?\n");
+	string   S1 = MyLib::AskString("Please  Enter your string?\n");
 
+	char   C = MyLib::AskChar("Please  Enter a character?\n");
 
 	cout << "\n";
 
-
-	cout << "\nString length = " << countLetters(s1);
-
-
-	cout << "\nCapital Letteres number = " << countLetters(s1, enWhatToCount::capitalLetter);
+	cout << "\nLetter = '" << C << "' Count = " << CountLetter( S1,  C);
 
 
-	cout << "\nLower Letter number = " << countLetters(s1, enWhatToCount::SmallLetters);
 
 
 
