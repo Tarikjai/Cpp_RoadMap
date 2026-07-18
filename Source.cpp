4fc996503de52c4208196bc998e16d08af4926cc
Tarik <tarik.jaidani@gmail.com>
2026-07-19 00:06:26 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -12,14 +12,15 @@ void countVowels(string   S1) {
 
 	for (int i = 0; i < S1.length(); i++) {
 		if (S1[i] != ' ') {
-			count++;
+			
 		}
 	}
 }
 
+
 int main() {
 
-	string   S1 = "Mohammed Abu-Hadhoud @ProgrammingAdvices";/* MyLib::AskString("Please  Enter a String?\n");*/
+	string   S1 = "Mohammed Abu-Hadhoud  @ProgrammingAdvices";/* MyLib::AskString("Please  Enter a String?\n");*/
 	cout << "Number of vowel is : " << countVowels(S1);
 
 }
