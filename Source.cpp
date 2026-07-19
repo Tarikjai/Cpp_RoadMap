bf0892320df5b87153ec1291ecf921f9a2abbaff
Tarik <tarik.jaidani@gmail.com>
2026-07-20 01:38:41 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -8,39 +8,39 @@ using namespace std;
 
 
 void PrintEachWordInString(string s1) {
+	cout << "Your String words are : " << endl;
 
 	string delim = " ";
-
 	short pos = 0;
 	string sWord;
 
-	cout << "Your String words are : " << endl;
-
-	while ((pos = s1.find(delim)) != std::string::npos) {
+	while ((pos = s1.find(delim)) != std::string::npos  ) {
 
 		sWord = s1.substr(0, pos);
+
 		if (sWord != "") {
 			cout << sWord << endl;
-
 		}
 
 		s1.erase(0, pos + delim.length());
 	}
 
 	if (s1 != "") {
-		cout << s1 << endl; 
+		cout << s1 << endl;
 	}
 
-	
+
 
 }
 
 
 
 
+
+
 int main() {
 
-	string   S1 = "Mohammed Abu-Hadhoud @ProgrammingAdvices";/* MyLib::AskString("Please  Enter a String?\n");*/
+	string   S1 = "Mohammed Abu-Hadhoud @ProgrammingAdvices   tabarani";/* MyLib::AskString("Please  Enter a String?\n");*/
 //	cout << "Your String words are : " << endl; 
 	PrintEachWordInString(S1);
 }
