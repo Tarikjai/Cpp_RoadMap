c4ab039921994fc8525d809b462a8e7c6a21073c
Tarik <tarik.jaidani@gmail.com>
2026-07-20 23:11:37 +0200

    push

---
+++ 35.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include "MyLib.h"
+#include <cctype>
+#include <vector>
+
+using namespace std;
+
+
+void PrintEachWordInString(string s1) {
+	cout << "Your String words are : " << endl;
+
+	string delim = " ";
+
+	
+
+}
+
+
+
+
+
+
+int main() {
+
+	string   S1 = "Mohammed Abu-Hadhoud @ProgrammingAdvices   tabarani";/* MyLib::AskString("Please  Enter a String?\n");*/
+	//	cout << "Your String words are : " << endl; 
+	PrintEachWordInString(S1);
+}
