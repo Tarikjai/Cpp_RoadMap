d2af8273ae202af9ef474ad8dce6041116207848
Tarik <tarik.jaidani@gmail.com>
2026-09-09 21:35:37 +0200

    first commit

--- 03_SumEachRows_InArrray.cpp
+++ 03_SumEachRows_InArrray.cpp
@@ -25,7 +25,6 @@ void PrintMatrix(int arr[3][3], int Rows, int Column) {
 		cout << endl;
 
 	}
-
 }
 
 int RowSum(int arr[3][3], int i, int Column) {
@@ -53,7 +52,7 @@ void SumMatrixRowsInArray(int arr[3][3], int Rows, int Column, int SumArr[3]) {
 
 void PrintRowsSumArray(int Arr[3], int Rows) {
 
-	cout << "\nThe following are the summ  for each row in the matrix :\n";
+	cout << "\nThe following are the summ  for each rows in the matrix :\n";
 	for (int i = 0; i < Rows; i++) {
 		cout <<"Row " << i + 1 << " = " <<  Arr[i];
 		cout << endl;
